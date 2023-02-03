4f48e751479eb96a8bf150575315cabcd29a8c89
Tajul Arifin Sirajudin <arifintajul4@gmail.com>
2023-02-03 17:02:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function arraySplit(array) {
-  let result = '';
-  for (let i = 0; i < array.length / 2; i++) {
-    result += `${array[i]} -> ${array[array.length - 1 - i]} -> ${Math.abs(array[i] - array[array.length - 1 - i])}\n`;
-  }
-  return result;
-}
+const array = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 20] 
+const middle = array.length / 2;
+let length = array.length-1;
 
-const arr = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 20];
-const output = arraySplit(arr);
-console.log(output);
+for (let i=0; i < length; i++)
+{
+  if(i < middle/2)
+  {
+    console.log(`${array[i]} -> ${array[(middle -1)-i]} -> ${array[i]}`)
+  }
+  
+  if(i >= middle && i <= (middle/2)+middle)
+  {
+    console.log(`${array[i]} -> ${array[length--]} -> ${array[array.length-1 - i]}`)
+  }
+}
